97a371c8ee836ad98339f5c473f9fcd5a860bd99
sdm-cmb <59738106+sdm-cmb@users.noreply.github.com>
2024-03-17 00:08:53 -0300

    Co-authored-by: Ronaldo Bueno <Buehno@users.noreply.github.com>

--- prj_cadastro/cadastro_produto.py
+++ prj_cadastro/cadastro_produto.py
@@ -9,7 +9,13 @@ class Produto:
         self.marca = marca_prod
         self.preco = preco_prod
 
-# a variavel abaixo é a variavel que roda a aplicação (todos os objetos de prjeto dependelm dela)
+prod01 = Produto("Camisa", "Nike", "R$300,00")
+prod02 = Produto("Blusa", "Lacoste", "R$255,00")
+prod03 = Produto("Calça", "Oakley", "R$200,00")
+
+produtos_cadastrados = [prod01, prod02, prod03]
+
+# a variavel abaixo é a variavel que roda a aplicação (todos os objetos de projeto dependem dela)
 app = Flask(__name__)
 #Varialvel da aplicação sempre pede __name__
 
@@ -18,37 +24,29 @@ app = Flask(__name__)
 def ola():
     return '<h1>Inciando Flask</h1>'
 
-@app.route('/lista')
+@app.route('/')
 def lista():
-
-# a linha abaixo instancia um novo produto
-    prod01 = Produto("Camisa", "Nike", "R$300,00")
-    prod02 = Produto("Blusa", "Lacoste", "R$255,00")
-    prod03 = Produto("Calça", "Oakley", "R$200,00")
-    
-    produtos_cadastrados = [prod01, prod02, prod03]
-
     return render_template('index.html',
-                           descricao ="Aqui estão seus produtos cadastrados", 
-                           lista_prod = produtos_cadastrados)
+                           descricao ="Aqui estão seus produtos cadastrados",  lista_prod = produtos_cadastrados)
 #render template busca a pagina dentro de uma pasta templates 
 
 
 @app.route('/cadastrar')
 def cadastrar_produto():
-    return render_template("cadastrar.html", methods="post" ,)
-    nome_prod: request.form['Txtnome']
-    marca_prod: request.form['Txtmarca']
-    preco_prod: request.form['Txtpreco']
+    return render_template('cadastrar.html')
 
-    novo_produto = Produto(nome_prod, 
-                           marca_prod,
-                             preco_prod)
-    
-    cadastrar_produto.append[novo_produto]
-    return render_template("lista.html",
-                           descricao= "Novo Produto Cadastrado",
-                           lista_prod = produtos_cadastrados)
+@app.route('/adicionar', methods=['POST', ])
+def adicionar_produtos():
+
+    nome_prod = request.form['TxtNome']
+    marca_prod = request.form['TxtMarca']
+    preco_prod = request.form['TxtPreco']
+
+    novo_produto = Produto(nome_prod, marca_prod, preco_prod)
+
+    produtos_cadastrados.append(novo_produto)
+    return render_template("index.html",
+                           descricao= "Novo Produto Cadastrado", lista_prod = produtos_cadastrados)
 app.run()
 
 #400 a 404 - pagina nao encontrada nos rastros 
